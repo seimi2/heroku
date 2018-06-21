2638d10b672c139e0648f901a85703ac61dedd39
seimi2 <40456079+seimi2@users.noreply.github.com>
2018-06-21 12:47:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,17 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const api = 'pFKZou1UIs5SPJoea1gONB1BMSycKg7g';
+
 const Discord = require("discord.js");
 
 
@@ -258,7 +272,6 @@ if(command === "ct") {
       .catch(error => message.reply(`Hata : ${error}`));
   }
 
-conts api = 'pFKZou1UIs5SPJoea1gONB1BMSycKg7g';
 client.on(`message`, async msg =>
     conts args = msg.content.split(" ").slice(1);
   
